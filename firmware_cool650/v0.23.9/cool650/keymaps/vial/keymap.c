b0179e8fec449aef926a030d004dd466cdb13602
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2024-12-08 07:48:18 +0900

    fix

--- firmware_cool650/v0.23.9/cool650/keymaps/vial/keymap.c
+++ firmware_cool650/v0.23.9/cool650/keymaps/vial/keymap.c
@@ -6,7 +6,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_ortho_5x5(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                              KC_1,    KC_2,    KC_3,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                              KC_H,    KC_J,    KC_K,    KC_L,    KC_MINUS,    KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                              KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_UP,  KC_SLSH, 
         KC_LCTL, KC_LALT, KC_LGUI, KC_INSERT, KC_SPACE,KC_SPACE,LT(1,KC_SPACE),LT(2,KC_SPACE),KC_SPACE, KC_SPACE, KC_RCTL,KC_LEFT, KC_DOWN, KC_RIGHT
